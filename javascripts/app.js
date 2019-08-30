02430ce8905c9109657ef275503a6b0c86145cf7
Veronica Donoso <veronicadonoso1506@gmail.com>
2019-08-30 11:26:08 -0400

    Project commands done

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,7 @@
 const rover = {
-  direction: 'E',
-  path: { x: 0, y: 0 },
+  direction: 'N',
+  x: 0,
+  y: 0,
   travelLog: []
 }
 const turn = ['N', 'W', 'S', 'E']
@@ -21,43 +22,64 @@ function turnRight(rover) {
   rover.direction = turn[(coordinate + 3) % turn.length]
 
   console.log(`Rover is facing now ${rover.direction}`)
-
 }
 
-function oneStep(rover) {
-  if (rover.direction === 'N') {
-    rover.path.y--
-    console.log(`The current position of rover is x:${rover.path.x}, y:${rover.path.y}`)
-  } else if (rover.direction === 'E') {
-    rover.path.x++
-    console.log(`The current position of rover is x:${rover.path.x}, y:${rover.path.y}`)
-  } else if (rover.direction === 'S') {
-    rover.path.y++
-    console.log(`The current position of rover is x:${rover.path.x}, y:${rover.path.y}`)
-  } else if (rover.direction === 'W') {
-    rover.path.x--
-    console.log(`The current position of rover is x:${rover.path.x}, y:${rover.path.y}`)
+function moveForward(rover) {
+  switch (rover.direction) {
+    case 'N':
+      rover.y--
+      break
+    case 'E':
+      rover.x++
+      break
+    case 'S':
+      rover.y++
+      break
+    case 'W':
+      rover.x--
+      break
   }
+  console.log(`The current position of rover is x:${rover.x}, y:${rover.y}`)
 }
 
-function commandsRover(commands) {
-  for (let i = 0; i < commands.length; i++) {
-    let command = commands[i]
-    switch (command) {
+function moveBackward(rover) {
+  switch (rover.direction) {
+    case 'N':
+      rover.y++
+      break
+    case 'E':
+      rover.x--
+      break
+    case 'S':
+      rover.y--
+      break
+    case 'W':
+      rover.x++
+      break
+  }
+  console.log(`The current position of rover is x:${rover.x}, y:${rover.y}`)
+}
+
+function commands (command) {
+  for (let i = 0; i < command.length; i++) {
+    var commandRover = command[i]
+
+    switch (commandRover) {
       case 'l':
         turnLeft(rover)
+        rover.travelLog.push(`x:${rover.x}, y:${rover.y}`)
         break
       case 'r':
         turnRight(rover)
+        rover.travelLog.push(`x:${rover.x}, y:${rover.y}`)
         break
       case 'f':
-        oneStep(rover)
+        moveForward(rover)
+        rover.travelLog.push(`x:${rover.x}, y:${rover.y}`)
         break
-      default:
-        console.log(`Please give rover the correct instructions l:left, r: right, f, forward`)
+      case 'b':
+        moveBackward(rover)
+        rover.travelLog.push(`x:${rover.x}, y:${rover.y}`)
     }
   }
-  console.log(rover.path)
 }
-
-var commands = 'flr'
